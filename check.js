e970c7da88fe250a3ff7be71795637e9c9cd46c1
0405200504 <luiseduardofariafilho@github.com>
2026-03-17 23:36:43 -0300

    Limpeza de arquivos e código

--- check.js
+++
@@ -1,8 +0,0 @@
-var fso = new ActiveXObject('Scripting.FileSystemObject');
-var strFile = fso.OpenTextFile('app.js', 1).ReadAll();
-try {
-   eval('function __syntax_test__() {' + strFile + '}');
-   WScript.Echo('SYNTAX OK');
-} catch(e) {
-   WScript.Echo('SYNTAX ERROR: ' + e.message);
-}
--- test_syntax.js
+++
@@ -1,9 +0,0 @@
-@
-var fso = new ActiveXObject('Scripting.FileSystemObject');
-var content = fso.OpenTextFile('app.js', 1).ReadAll();
-try {
-  eval('function test(){' + content + '}');
-  WScript.Echo('Syntax OK');
-} catch(e) {
-  WScript.Echo('Error: ' + e.description);
-}
